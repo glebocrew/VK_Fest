d767ac98f1c73fd012240659251ce097adf94a62
glebocrew <62657311+glebocrew@users.noreply.github.com>
2025-07-08 16:16:33 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,98 @@
+from telebot import *
+from telebot.types import InlineKeyboardButton, InlineKeyboardMarkup
+from res.scr.ops import *
+
+
+CONF_PATH = "res/conf/conf.json"
+API = "7682725450:AAHNo29JdS75RPiZjSzNdkK5VHhN_qP9McU"
+ADMINS = [1433192741]
+
+configures = read_json(CONF_PATH)
+# print(configures)
+
+mariaconnection = MariaConnection(args=configures["database"])
+users = Users(mariaconnection)
+
+bot = TeleBot(API)
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    if users.get_by_username(message.chat.username) != None:
+        bot.send_message(message.chat.id, text=configures["phrazes"]["start"])
+    else:
+        bot.send_message(message.chat.id, text=configures["phrazes"]["email"])
+        bot.register_next_step_handler_by_chat_id(message.chat.id, email)
+    
+
+        
+def email(message):
+    if "@" in message.text:
+        users.insert_with_username(message.chat.username, message.chat.id, message.text)
+        bot.send_message(message.chat.id, text=configures["phrazes"]["correct-email"])
+    else:
+        bot.send_message(message.chat.id, text=configures["phrazes"]["incorrect-email"])
+        bot.send_message(message.chat.id, text=configures["phrazes"]["email"])
+        bot.register_next_step_handler_by_chat_id(message.chat.id, email)
+
+@bot.message_handler(commands=["game"])
+def game(message):
+    keyboard = InlineKeyboardMarkup()
+
+    for theme in configures["paths"]:
+        btn = InlineKeyboardButton(text=theme, callback_data=theme)
+        keyboard.add(btn)
+    
+    bot.send_message(message.chat.id, text=configures["phrazes"]["game"], reply_markup=keyboard)
+        
+@bot.callback_query_handler(func=lambda call: True)
+def callback(call):
+    if call.data in configures["paths"].keys():
+        keyboard = InlineKeyboardMarkup()
+
+        for level in configures["paths"][call.data]:
+            btn = InlineKeyboardButton(text=level, callback_data=f"{call.data}_{level}")
+            keyboard.add(btn)
+
+        bot.send_message(call.message.chat.id, text=configures["phrazes"]["level"], reply_markup=keyboard)
+    
+    elif "_" in call.data:
+        path = call.data.split(sep="_")
+        photo = open(configures["pngs"] + configures["paths"][path[0]][path[1]], "rb")
+        bot.send_photo(call.message.chat.id, photo=photo)
+        photo.close()
+
+@bot.message_handler(commands=["admin"])
+def admin(message):
+    if message.chat.id in ADMINS:
+        bot.send_message(message.chat.id, text=configures["phrazes"]["admin"])
+        bot.register_next_step_handler_by_chat_id(message.chat.id, admin_forward)
+
+
+def admin_forward(message):
+    # print(users.get_all_telegramms())
+    for user_id in users.get_all_telegramms():
+        # print(user_id)
+        try:
+            bot.copy_message(
+            chat_id=int(user_id[0]),
+            from_chat_id=message.chat.id,
+            message_id=message.message_id
+            )
+        except Exception as e:
+            print(e)
+
+@bot.message_handler(commands=["materials"])
+def materials(message):
+    bot.send_message(message.chat.id, text=configures["phrazes"]["materials_pw"])
+    bot.register_next_step_handler_by_chat_id(message.chat.id, send_materials)
+
+def send_materials(message):
+    if str(message.text) == str(configures["materials"]["password"]):
+        bot.send_message(message.chat.id, text=configures["materials"]["materials"])
+    else:
+        bot.send_message(message.chat.id, text=configures["phrazes"]["incorrect_materials_pw"])
+
+
+if __name__ == "__main__":
+    bot.infinity_polling()
---
+++ res/scr/ops.py
@@ -0,0 +1,66 @@
+from mariadb import *
+from json import load
+import mariadb
+
+def read_json(filepath:str):
+    """
+    Reads json loacated in filepath
+    """
+    with open(mode="r", file=filepath, encoding="utf-8") as conf:
+        return load(conf)
+
+
+class NoMariaArgs(Exception):
+    def __init__(self):
+        return "MariaConnection object has no args while creating"
+
+
+class MariaConnection:
+    def __init__(self, args:dict):
+        """
+        Creates mariadb connection
+        :param args: should be host, port, user, password, db
+        """
+        if args != {}:
+            self.mariaconnection = mariadb.connect(**args)
+            self.cursor = Cursor(self.mariaconnection)
+            self.mariaconnection.autocommit = True
+        else:
+            raise NoMariaArgs
+        
+    def get_conn(self):
+        return self.mariaconnection
+    
+    def get_curr(self):
+        return self.cursor
+    
+
+class Users:
+    def __init__(self, mariaconnection: MariaConnection):
+        """
+        Table users ops here
+        :param mariaconnection: MariaConnection object
+        """
+        self.mariaconnection = mariaconnection
+        self.queries = {
+            "get_by_username": "SELECT * FROM users WHERE username = ?;",
+            "get_by_email": "SELECT * FROM users WHERE email = ?;",
+            "insert_with_username": "INSERT INTO users (username, id, email) VALUES (?, ?, ?);",
+            "get_all_ids": "SELECT id FROM users;"
+        }
+
+    def get_by_username(self, username:str):
+        self.mariaconnection.cursor.execute(self.queries["get_by_username"], [username])
+        return self.mariaconnection.cursor.fetchone()
+
+    def get_by_email(self, email:str):
+        self.mariaconnection.cursor.execute(self.queries["get_by_email"], [email])
+        return self.mariaconnection.cursor.fetchone()
+        
+    
+    def insert_with_username(self, username:str, id:str, email:str):
+        self.mariaconnection.cursor.execute(self.queries["insert_with_username"], [username, id, email])
+
+    def get_all_telegramms(self):
+        self.mariaconnection.cursor.execute(self.queries["get_all_ids"])
+        return self.mariaconnection.cursor.fetchall()
